5b722dbbeccc143995f16a4f8d9644fcf9a4a826
DESKTOP-KVCQHCD\SSAFY <seulgi9834@naver.com>
2022-11-02 11:42:12 +0900

    Create: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/starrynight/StarrynightApplication.java
@@ -0,0 +1,13 @@
+package starrynight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarrynightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarrynightApplication.class, args);
+	}
+
+}
