aa2cce08e1ebbe943e2dad8291c93e048d3a8971
heejo <joy96817@naver.com>
2022-11-14 16:07:12 +0900

    Fix: 게임 초기 설정 관련 오류 해결(초기 DB 설정이 회원가입이 아닌 로그인마다 작동되는 문제 해결)

--- backend/src/main/java/starrynight/config/auth/OAuthService.java
+++ backend/src/main/java/starrynight/config/auth/OAuthService.java
@@ -68,8 +68,15 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
                 .map(m -> m.update(memberProfile.getName(), memberProfile.getEmail())) // OAuth 서비스 사이트에서 유저 정보 변경이 있을 수 있기 때문에 우리 DB에도 update
                 .orElse(memberProfile.toMember());
 
-        memberRepository.save(member);
-        gameService.setInitialGameInfor(member);
+        //DB에 멤버가 이미 존재한다면
+        if(memberRepository.existsById(member.getId())){
+            memberRepository.save(member);
+        }
+        else{
+            memberRepository.save(member);
+            gameService.setInitialGameInfor(member);
+        }
+
         return member;
     }
 
