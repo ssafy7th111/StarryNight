64b28a5319b317ce91768c97488a8156c8f5807b
heejo <joy96817@naver.com>
2022-11-14 09:41:22 +0900

    Feat: 회원가입 시 스토리 목록 및 스타코인 목록 초기 설정

--- backend/src/main/java/starrynight/config/auth/OAuthService.java
+++ backend/src/main/java/starrynight/config/auth/OAuthService.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import starrynight.api.service.GameService;
 import starrynight.db.entity.Member;
 import starrynight.db.repository.MemberRepository;
 
@@ -23,6 +24,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final MemberRepository memberRepository;
+    final GameService gameService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -66,7 +68,9 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
                 .map(m -> m.update(memberProfile.getName(), memberProfile.getEmail())) // OAuth 서비스 사이트에서 유저 정보 변경이 있을 수 있기 때문에 우리 DB에도 update
                 .orElse(memberProfile.toMember());
 
-        return memberRepository.save(member);
+        memberRepository.save(member);
+        gameService.setInitialGameInfor(member);
+        return member;
     }
 
 }
